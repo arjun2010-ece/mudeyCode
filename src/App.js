9a0bd997b024a7419102e505f11f267e8b2173c1
arjun2010.ece <arjun2010.ece@gmail.com>
2020-01-17 03:42:59 +0000

    tried replicating js to react but failed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {apiData} from './data/api';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const App = () => {
+  const [data, setData] = useState(apiData);
+
+  const unflatten = indata => {
+    let nodes;
+    if(indata){
+     nodes = indata.sort((a, b) => a.id - b.id)
+      .reduce((a, e) => {
+        a[e.id] = a[e.id] || e;
+        a[e.parent_id] = a[e.parent_id] || {};
+        const parent = a[e.parent_id];
+        parent.children = parent.children || [];
+        parent.children.push(e);
+        return a;
+      }, {});
+   
+    if(nodes){
+        return Object.values(nodes)
+        .find(e => e.id === undefined).children;
+    }
+  }
+  };
+
+  return (
+    <div className="container">
+
+       <h2>Second list touched or manipulated</h2>
+       { JSON.stringify(unflatten()) }
+      </div>
+  )
+}
 export default App;
---
+++ src/data/api.js
@@ -0,0 +1,41 @@
+export const apiData = [{
+    "id": 1,
+    "label": "List item 1",
+    "parent_id": 0
+  },
+  {
+    "id": 9,
+    "label": "List item 1",
+    "parent_id": 8
+  },
+  {
+    "id": 8,
+    "label": "List item 1",
+    "parent_id": 1
+  },
+  {
+    "id": 5,
+    "label": "List item 1",
+    "parent_id": 1
+  },
+  {
+    "id": 6,
+    "label": "List item 1",
+    "parent_id": 1
+  },
+  {
+    "id": 7,
+    "label": "List item 1",
+    "parent_id": 1
+  },
+  {
+    "id": 10,
+    "label": "List item 1",
+    "parent_id": 8
+  },
+  {
+    "id": 2,
+    "label": "List item 1",
+    "parent_id": 0
+  }
+];
